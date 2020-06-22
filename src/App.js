ea0ce377075193f8c79b1dbcbf284117b9fb9c98
Tautvydas <tautvydas.kvietkauskas@gmail.com>
2020-06-22 22:47:48 +0300

    presentational and Container components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponents';
 import './App.css';
-import {DISHES} from './components/shared/dishes';
+import Main from './components/MainComponent';
 
+class App extends Component {
 
-class App extends Component{
-  constructor(props)
-  {
-    super(props);
-    
-      this.state={
-        dishes:DISHES
-      };
-  }
-  
-  render(){
+  render() {
     return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/"> 
-              Ristorante Con Fusion
-            </NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes}/>
+      <div className="App">
+        <Main />
       </div>
     );
   }
 }
 
- 
 export default App;
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -32,8 +32,7 @@ class Dishdetail extends Component {
                     <p>{comment.comment}</p>
                     <p>-- {comment.author},
                         &nbsp;
-                        {comment.date}
-                    </p>
+                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                 </li>
             )
         })
@@ -55,9 +54,11 @@ class Dishdetail extends Component {
             const dishItem = this.renderDish(dish)
             const commentItem = this.renderComments(dish.comments)
                 return (
+                <div className='container'>
                 <div className='row'>
                     {dishItem}
                     {commentItem}
+                </div>
                 </div>
             );
         }
