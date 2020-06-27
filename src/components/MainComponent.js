338c665600995659b822f28cf73e753f158b38c8
Tautvydas <tautvydas.kvietkauskas@gmail.com>
2020-06-27 18:12:31 +0300

    Single Page aplicartion part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
-import Footer from './FooterComponent';
 import { DISHES } from './shared/dishes';
+import { COMMENTS } from './shared/comments';
+import { LEADERS } from './shared/leaders';
+import { PROMOTIONS } from './shared/promotions';
+import Footer from './FooterComponent';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
 
@@ -11,20 +17,33 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null
+        comments:COMMENTS,
+        leaders:LEADERS,
+        promotions:PROMOTIONS  
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
-
   render() {
+
+const HomePage = () => {
+  return(
+    <Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
+    promotion={this.state.promotions.filter((promo)=>promo.featured)[0]}
+    leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
+    />
+    
+  );
+}
+
     return (
       <div>
-        <Header/>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Header/>     
+        <Switch>
+              <Route path="/home" component={HomePage} />
+              <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path="/contactus" component={Contact}/>
+              <Redirect to="/home" />
+        </Switch>
         <Footer/>
       </div>
     );
