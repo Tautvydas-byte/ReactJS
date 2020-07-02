e5311003d06f54a0f084893e90561c788bf1f99b
Tautvydas <tautvydas.kvietkauskas@gmail.com>
2020-07-02 23:20:09 +0300

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,25 @@
-/*nebutinai taip daryti, bet jis taip pasirinko daryti su configureStoe.js */
-import {createStore} from 'redux';
-import { Reducer, initialState } from './reducer'
+/*nebutinai taip daryti, bet jis taip pasirinko daryti su configureStore.js */
+import {createStore, combineReducers} from 'redux';
+//import { Reducer, initialState } from './reducer'
+import { Dishes} from './dishes';
+import { Comments} from './comments';
+import { Promotions} from './promotions';
+import { Leaders} from './leaders';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
 
 export const ConfigureStore = () => {/*butonai reikia configuruoti  funkcija cretina redux store*/
     const store = createStore(
-        Reducer, // reducer /*sukuriami du parametrai*/
-        initialState, // our initialState
+        //Reducer, // reducer /*sukuriami du parametrai*/
+        //initialState, // our initialState
+        combineReducers({/*apibreziam kaip reikia combineReducer, kuriuos defined here (importinom) */
+/*Recomposed the global state for our application. Cia yra isskirti keturiuose skirtinguose failuose reduceriai ir sitoje funkcijoje sujungiame i viena */
+            dishes:Dishes,
+            comments:Comments,
+            promotions:Promotions,
+            leaders:Leaders
+        })
     );
 
     return store;
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,6 @@
-import { DISHES } from '../shared/dishes';
+//kadangi reducer.js pagal Combining Reducers isskirstem i 4-is skirtingus js failus(dishes,comments,promotions,leaders), tai sis failas tampa nereikalingas
+
+/*import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -10,6 +12,6 @@ export const initialState = {
     leaders: LEADERS
 };
 
-export const Reducer = (state = initialState /*if state value undefine tai default value bus initialState */, action) => {
+export const Reducer = (state = initialState //if state value undefine tai default value bus initialState , action) => {
     return state;
-};
+};*/
